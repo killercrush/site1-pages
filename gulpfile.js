2e534ad3243d0d8b527a147094a76c1158f0d392
killercrush <falovskii-artur@mail.ru>
2016-05-25 14:41:52 +0600

    header start

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var minify = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-  return gulp.src('./src/**/*.scss')
+  return gulp.src('./src/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(minify({compatibility: 'ie8'}))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 
  
 gulp.task('pug', function buildHTML() {
-  return gulp.src('./src/**/*.pug')
+  return gulp.src('./src/pug/*.pug')
   .pipe(pug({
     // Your options in here. 
   }))
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('./dist'));
 });
  
 // gulp.task('sass:watch', function () {
