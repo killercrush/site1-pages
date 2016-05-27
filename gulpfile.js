a9e10eb70d8b2536332bccfff5f802e9c2306260
killercrush <falovskii-artur@mail.ru>
2016-05-27 12:33:20 +0600

    add carousel

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var minify = require('gulp-clean-css');
 var rename = require('gulp-rename');
-
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 gulp.task('sass', function () {
   return gulp.src('./src/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
@@ -21,7 +22,15 @@ gulp.task('pug', function buildHTML() {
   }))
   .pipe(gulp.dest('./dist'));
 });
- 
+
+gulp.task('owl', function () {
+  return gulp.src(['./src/jslib/jquery.js', 
+  	'./src/jslib/owl.carousel.js', 
+  	'./src/js/carousel-init.js'])
+	.pipe(concat('script.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./dist/js'));	
+});
 // gulp.task('sass:watch', function () {
 //   gulp.watch('./sass/**/*.scss', ['sass']);
 // });
