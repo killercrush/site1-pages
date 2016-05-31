74c9f6c07224c7f528dce51fb135eb21cd4335a9
killercrush <falovskii-artur@mail.ru>
2016-05-31 22:31:58 +0500

    product

--- src/js/carousel-init.js
+++ src/js/carousel-init.js
@@ -17,7 +17,11 @@ function changeColor(id) {
   $(this).addClass("color-selector__thumb--selected");
   document.querySelector("#item_" + id + " .catalog-item__thumb").style.backgroundImage=this.style.backgroundImage;
 }
-
+function changeView() {
+  $(this).siblings().removeClass("preview__thumb--selected");
+  $(this).addClass("preview__thumb--selected");
+  document.querySelector("#view").src = this.src;
+}
 function changeTab(id) {
   $(this).addClass("tabs__control--active");
   $(this).siblings().removeClass("tabs__control--active"); 
